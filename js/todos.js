d75f62d604a6d1ef9070d52e091e523e920c8422
fgelsomini <fgelsomini@gmail.com>
2016-04-10 08:48:11 -0400

    Delete todos.js

--- js/todos.js
+++
@@ -1,7 +0,0 @@
-// Write a function that updates the counter based on which todos are completed
-
-
-// Run the function you just wrote to update the counter
-
-
-// Add an event listener to each of the todos which will toggle the 'is-complete' css class when clicked
